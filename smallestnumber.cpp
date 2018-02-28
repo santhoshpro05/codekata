a7ce77a93bdfdb33f39885a19fac49d24d6bf22e
santhoshpro05 <36360453+santhoshpro05@users.noreply.github.com>
2018-02-28 17:53:43 +0530

    Update smallestnumber.cpp

--- smallestnumber.cpp
+++ smallestnumber.cpp
@@ -7,15 +7,13 @@ int ksmallest(int arr[], int n, int k)
     memset(b, 0, sizeof b);
     for (int i = 0; i < n; i++)
         b[arr[i]] = 1;
- 
-    for (int j=1; j<MAX; j++)
+ for (int j=1; j<MAX; j++)
     {
         if (b[j] != 1)
             k--;
         if (!k)
             return j;
-    }
-}
+    }}
 int main()
 {
     int k = 1;
