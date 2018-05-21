2d98e92dbeb2ec8cd850cc4479d3aa24417cfab1
santhoshpro05 <36360453+santhoshpro05@users.noreply.github.com>
2018-05-21 13:53:43 +0530

    Create arraywithindex.cpp

---
+++ arraywithindex.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int Arr[5],i,n;
+ cout<<"enter number";
+ cin>>n;
+ cout<<"elements";
+ for(i=0;i<n;i++)
+ {
+ 	cin>>Arr[i];
+ 	cout<<i<<Arr[i];
+ }
+
+	return 0;
+}
