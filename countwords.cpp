88996ad3117663ffe0f9ee995a836d6833485cb8
santhoshpro05 <36360453+santhoshpro05@users.noreply.github.com>
2018-05-23 12:22:31 +0530

    Create countwords.cpp

---
+++ countwords.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ char a[10];
+    int count = 0,i;
+  cin>>a;
+    for(i = 1;a[i]!='\0';i++)
+    {
+        if(!isspace (str[i]))
+        
+        count++;
+      
+    }
+    cout<<count;
+    return 0;
+}
