eb16d04471ffcfa7c9bc652e3e1bbcd9f2054b2d
santhoshpro05 <36360453+santhoshpro05@users.noreply.github.com>
2018-05-23 11:39:56 +0530

    Create hrssubtraction.cpp

---
+++ hrssubtraction.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int hrs1,hrs2,min1,min2,min,hrs;
+cin>>hrs1>>min1>>hrs2>>min2;
+hrs=hrs1-hrs2;
+cout<<hrs<<'\t';
+min=min1-min2;
+cout<<min<<'\t';
+return 0;
+}
